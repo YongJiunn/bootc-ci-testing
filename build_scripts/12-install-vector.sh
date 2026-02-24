f550b3121033329aa77b7607dfd8779a89d5b204
YongJiunn <62463250+YongJiunn@users.noreply.github.com>
2026-02-24 19:29:55 +0800

    push test

--- build_scripts/12-install-vector.sh
+++ build_scripts/12-install-vector.sh
@@ -1,26 +1,49 @@
+# #!/bin/bash
+# set -xeuo pipefail
+
+# #curl --proto '=https' --tlsv1.2 -sSfL https://sh.vector.dev | bash -s -- -y
+
+# url=$(curl -s https://api.github.com/repos/vectordotdev/vector/releases | \
+#   jq -r 'sort_by(.published_at) | last | .assets[] | select(.name | test(".*x86_64-unknown-linux-musl.*")) | .browser_download_url')
+
+# mkdir -p vector && \
+#   curl -sSfL --proto '=https' --tlsv1.2 "$url"  | \
+#   tar xzf - -C vector --strip-components=2
+
+# #ls vector
+
+# # move to /usr/bin
+# mv vector/bin/vector /usr/bin/vector && chmod +x /usr/bin/vector
+
+# #echo "export PATH=\"$(pwd)/vector/bin:\$PATH\"" >> $HOME/.profile
+# #source $HOME/.profile
+
+# ## add symlink to make it run at startup
+# ln -s /usr/lib/systemd/system/vector.service /etc/systemd/system/multi-user.target.wants/vector.service 
+
+# # replace the version
+# sed -i "s/POSTGRESQL_MAJOR_VERSION/$POSTGRESQL_MAJOR_VERSION/g" /etc/vector/vector.yaml
+
 #!/bin/bash
 set -xeuo pipefail
 
-#curl --proto '=https' --tlsv1.2 -sSfL https://sh.vector.dev | bash -s -- -y
+VECTOR_VERSION="${VECTOR_VERSION:-0.38.0}"
+ARCH="x86_64-unknown-linux-musl"
 
-url=$(curl -s https://api.github.com/repos/vectordotdev/vector/releases | \
-  jq -r 'sort_by(.published_at) | last | .assets[] | select(.name | test(".*x86_64-unknown-linux-musl.*")) | .browser_download_url')
+FILENAME="vector-${VECTOR_VERSION}-${ARCH}.tar.gz"
+URL="https://github.com/vectordotdev/vector/releases/download/v${VECTOR_VERSION}/${FILENAME}"
 
-mkdir -p vector && \
-  curl -sSfL --proto '=https' --tlsv1.2 "$url"  | \
-  tar xzf - -C vector --strip-components=2
+mkdir -p vector
 
-#ls vector
+curl -sSfL --proto '=https' --tlsv1.2 "$URL" -o "$FILENAME"
 
-# move to /usr/bin
-mv vector/bin/vector /usr/bin/vector && chmod +x /usr/bin/vector
+tar xzf "$FILENAME" -C vector --strip-components=2
 
-#echo "export PATH=\"$(pwd)/vector/bin:\$PATH\"" >> $HOME/.profile
-#source $HOME/.profile
+mv vector/bin/vector /usr/bin/vector
+chmod +x /usr/bin/vector
 
-## add symlink to make it run at startup
-ln -s /usr/lib/systemd/system/vector.service /etc/systemd/system/multi-user.target.wants/vector.service 
+ln -sf /usr/lib/systemd/system/vector.service \
+  /etc/systemd/system/multi-user.target.wants/vector.service
 
-# replace the version
-sed -i "s/POSTGRESQL_MAJOR_VERSION/$POSTGRESQL_MAJOR_VERSION/g" /etc/vector/vector.yaml
-
+sed -i "s/POSTGRESQL_MAJOR_VERSION/${POSTGRESQL_MAJOR_VERSION}/g" \
+  /etc/vector/vector.yaml
