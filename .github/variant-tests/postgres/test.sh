5d9be3dfd8482e5364f95a6d8770a3836fea567a
YongJiunn <62463250+YongJiunn@users.noreply.github.com>
2026-04-09 14:06:23 +0800

    Update test.sh

--- .github/variant-tests/postgres/test.sh
+++ .github/variant-tests/postgres/test.sh
@@ -4,5 +4,6 @@ set -xeuo pipefail
 echo "Executing custom tests for Postgres Variant!"
 # Example: systemctl is-active postgresql-17
 # Example: psql -c "SELECT 1;"
+systemctl is-active postgresql-17.service
 
 exit 0
