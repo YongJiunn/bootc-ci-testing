582285d03c5cf908ce29ba6077f6fa2c1d721c0f
Yong Jiun Lew <yongjiun.lew@gmail.com>
2026-04-20 00:07:26 +0800

    testing

--- .github/variant-tests/postgres/test.sh
+++ .github/variant-tests/postgres/test.sh
@@ -13,6 +13,7 @@ echo "Waiting for PostgreSQL service to fully initialize..."
 timeout 60 bash -c 'until systemctl is-active postgresql-17.service; do sleep 2; echo "Still waiting for initdb..."; done'
 
 echo "PostgreSQL is active! Proving database connection..."
-sudo -u postgres psql -c "SELECT 1;"
+# sudo -u postgres psql -c "SELECT 1;"
+runuser -u postgres -- psql -c "SELECT 1;"
 
 exit 0
